26d5cbc5bef352b50370fba08e1c4c9bf5498e09
asleidymurillo07 <asleidymurillo07@gmail.com>
2026-09-22 21:22:35 -0700

    Usar dj_database_url y DATABASE_URL para el pooler

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
+
 
 from dotenv import load_dotenv
 
@@ -82,14 +84,11 @@ load_dotenv()
 # ... (deja el resto del código tal como está hasta llegar a DATABASES)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
